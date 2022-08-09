7426416543c936ceacdd86806f5d150750f801fb
doyeoo <ehdusla013@gmail.com>
2022-08-09 22:47:55 +0900

    Fix: CORS ORIGIN 경로 수정

--- overtheott/settings.py
+++ overtheott/settings.py
@@ -59,7 +59,7 @@ REST_FRAMEWORK = {
 
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
-    'https://over-the-ott.herokuapp.com/',
+    'https://over-the-ott.herokuapp.com',
     'http://127.0.0.1:8000',
     'http://localhost:3000',
     'http://127.0.0.1:3000',
