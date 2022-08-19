9df05194140bed514137a4f364c28e61aeff67b7
doyeoo <ehdusla013@gmail.com>
2022-08-19 17:47:11 +0900

    Feat: 도메인 추가

--- overtheott/settings.py
+++ overtheott/settings.py
@@ -76,6 +76,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
     'https://gchaewon.github.io',
+    'https://over-the-ott-front.vercel.app',
 ]
 
 MIDDLEWARE = [
